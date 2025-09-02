a16b5e10536aa9fba782484c5908838b9caa0d61
Ivyliz25 <kish.liz10349@gmail.com>
2025-09-02 12:56:30 +0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,11 +1,13 @@
--- Users already created
+CREATE DATABASE recipe_app;
+USE recipe_app;
+
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL
 );
 
--- Recipes with foreign key
+
 CREATE TABLE recipes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     ingredients TEXT NOT NULL,
